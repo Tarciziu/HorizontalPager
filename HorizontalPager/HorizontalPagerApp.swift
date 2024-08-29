55901ba3bad2a5f6f86ba3f621823a8ef1592f22
Tarciziu <tarciziu@Tarcizius-MacBook-Air.local>
2024-08-29 22:56:05 +0300

    Set up project  - Set up initial project
    Type: Setup

---
+++ HorizontalPager/HorizontalPagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalPagerApp.swift
+//  HorizontalPager
+//
+//  Created by Tarciziu Gologan on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalPagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
